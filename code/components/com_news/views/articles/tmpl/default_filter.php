936085c9dad0b66775dee1d9dd74c2dc97da4261
Tom Janssens <tom@timble.net>
2012-06-13 14:48:38 +0200

    re #1 Improved UI of calendar behavior

--- code/components/com_news/views/articles/tmpl/default_filter.php
+++ code/components/com_news/views/articles/tmpl/default_filter.php
@@ -3,25 +3,22 @@
 		<?= @template('com://site/news.view.categories.list', array('categories' => @service('com://site/news.model.categories')->getList()))?>
 	</div>
 	<form style="float: right;" action="" method="get" class="-koowa-form form-inline">			
-		<label><?= @text('From') ?></label>
-		<?= @helper('behavior.calendar',
+		<div class="input-prepend input-append">
+		<span class="add-on"><?=@text( 'From' )?></span><?= @helper('behavior.calendar',
 				array(
 					'date' => $state->start_date,
 					'name' => 'start_date',
 					'format' => '%d-%m-%Y',
 					'attribs' => array('style' => 'width: 70px')
-				)); ?>
-		<label><?=@text( 'till' )?></label>
-		<?= @helper('behavior.calendar',
+				)); ?><span class="add-on"><?=@text( 'till' )?></span><?= @helper('behavior.calendar',
 				array(
 					'date' => $state->end_date,
 					'name' => 'end_date',
 					'format' => '%d-%m-%Y',
 					'attribs' => array('style' => 'width: 70px')
-				)); ?>				
-		<input class="btn btn-mini" type="submit" value="<?= @text('Go') ?>" /> <?=@text( 'or' )?>
-		<a href="<?= @route('&start_date=&end_date=') ?>">
-		   Reset
-		</a>
+				)); ?><input class="btn" type="submit" value="<?= @text('Go') ?>" /><a class="btn" href="<?= @route('&start_date=&end_date=') ?>">
+				   <i class="icon-remove"></i>
+				</a>
+		</div>
 	</form>
 </module>
