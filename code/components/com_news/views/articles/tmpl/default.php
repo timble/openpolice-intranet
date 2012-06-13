388c970bee6a4fcd9f23872ecc66f998dabf4477
Tom Janssens <tom@timble.net>
2012-06-13 17:10:03 +0200

    re #5 Moved new button to sidebar

--- code/components/com_news/views/articles/tmpl/default.php
+++ code/components/com_news/views/articles/tmpl/default.php
@@ -1,15 +1,4 @@
 <div class="articles">
-	<? if($agent) : ?>
-	<div class="articles-toolbar">
-	    <? if($state->category) : ?>
-	    <a class="btn btn-primary btn-small" href="<?= @route('view=article&layout=form&category='.$state->category) ?>">
-	    <? else : ?>
-	    <a class="btn btn-primary btn-small disabled" href="#">
-	    <? endif ?>
-	        <i class="icon-plus icon-white"></i> <?= @text('New') ?>
-	    </a>
-	</div>
-	<? endif ?>
 	<? foreach($articles as $article) : ?>
 		<div class="article">
 			<?= @template('com://site/news.view.article.default_article', array('article' => $article)) ?>
@@ -26,6 +15,20 @@
 	<?= @template('com://site/news.view.grid.search', array('state' => $state)) ?>
 </module>
 
-<module title="" position="sidebar">
+<? if($agent) : ?>
+<module title="<?= @text('') ?>" position="sidebar">
+<div class="articles-toolbar">
+    <? if($state->category) : ?>
+    <a style="width: 90%;" class="btn btn-primary btn-small" href="<?= @route('view=article&layout=form&category='.$state->category) ?>">
+    <? else : ?>
+    <a style="width: 90%;" class="btn btn-primary btn-small disabled" href="#">
+    <? endif ?>
+        <i class="icon-plus icon-white"></i> <?= @text('New') ?>
+    </a>
+</div>
+</module>
+<? endif ?>
+
+<module title="<?= @text('Calendar') ?>" position="sidebar">	
 	<?= @service('mod://site/calendar.html')->display(); ?>
 </module>
