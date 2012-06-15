dac3b3e6bd5e28c6399410ab49a5de5552acd1d4
Steven Rombauts <steven@timble.net>
2012-06-15 12:20:44 +0200

    re #12 - implement activity stream

--- code/components/com_news/aliases.php
+++ code/components/com_news/aliases.php
@@ -1,5 +1,7 @@
 <?php
 KService::setAlias('com://site/news.database.table.articles', 'com://admin/news.database.table.articles');
 
+KService::setAlias('com://site/news.model.activities', 'com://admin/news.model.activities');
+
 KService::setAlias('com://site/news.model.articles', 'com://admin/news.model.articles');
 KService::setAlias('com://site/news.model.comments', 'com://admin/news.model.comments');
