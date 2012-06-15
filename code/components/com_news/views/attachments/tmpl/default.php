cfdc280ec5b030aa4d29d88fb636f6e1622823da
Tom Janssens <tom@timble.net>
2012-06-15 18:25:58 +0200

    re #5 Styled thumbnails in attachments

--- code/components/com_news/views/attachments/tmpl/default.php
+++ code/components/com_news/views/attachments/tmpl/default.php
@@ -3,15 +3,17 @@
 <? $list = (isset($row) && isset($table)) ? $attachments->find(array('row' => $row, 'table' => $table)) : $attachments ?>
 
 <? if(count($list)) : ?>
-    <div class="attachments-gallery">
+    <ul class="attachments-gallery thumbnails">
     <? foreach($list as $item) : ?>
     	<? if($item->file->isImage()) : ?>
-    	<a href="<?= @route('view=attachment&format=file&id='.$item->id) ?>" class="modal" rel="{handler: 'image'}">
-    	   <img src="<?= $item->thumbnail->thumbnail ?>" />
-    	</a>
+    	<li style="width: 125px;">
+	    	<a class="thumbnail" href="<?= @route('view=attachment&format=file&id='.$item->id) ?>" class="modal" rel="{handler: 'image'}">
+	    	   <img src="<?= $item->thumbnail->thumbnail ?>" />
+	    	</a>
+    	</li>
     	<? endif ?>
     <? endforeach ?>
-	</div>
+	</ul>
     
     <ul class="attachments-list">
     <? foreach($list as $item) : ?>        
