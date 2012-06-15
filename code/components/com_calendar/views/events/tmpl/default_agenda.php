0afdd41292462789d683a60bc523db788c55734b
Tom Janssens <tom@timble.net>
2012-06-15 11:57:37 +0200

    re #15 Added proper filtering based on start date and moved 'new' button to sidebar

--- code/components/com_calendar/views/events/tmpl/default_agenda.php
+++ code/components/com_calendar/views/events/tmpl/default_agenda.php
@@ -1,18 +1,22 @@
 <table class="table table-striped">
 	<thead>
-		<th width="10%">Date</th>
-		<th width="10%">Time</th>
-		<th width="80%">Event</th>
+		<th width="15%">Date</th>
+		<th width="15%">Time</th>
+		<th width="70%">Event</th>
 	</thead>
 	<tbody>
 		<? foreach($events as $event) : ?>
 		<tr>
 			<td><?= @helper('date.format', array('date' => $event->start_date, 'format' => '%d %b')) ?></td>
 			<td><?= @helper('date.format', array('date' => $event->start_date, 'format' => '%H:%M')) ?></td>
-			<td><?= $event->title ?>
-				<div style="float: right;">
+			<td>
+				<?= $event->title ?>
+				<? if($agent) : ?>
+				<div class="btn-group" style="float: right;">
 					<a class="btn  btn-mini" href="<?= @route('view=event&layout=form&id='.$event->id) ?>"><i class="icon-pencil"></i> <?= @text('Edit') ?></a>
+					<a class="btn  btn-mini btn-danger" href="#"><i class="icon-remove icon-white"></i> <?= @text('Delete') ?></a>
 				</div>
+				<? endif ?>
 			</td>
 		</tr>
 		<? endforeach ?>
