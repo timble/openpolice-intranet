b79e07e1b83032a0805895f9f5bc8713337e5fa4
Tom Janssens <tom@timble.net>
2012-06-15 12:24:30 +0200

    re #1 Fixed commentable setting, overriding template helper to match bootstrap markup

--- code/administrator/components/com_news/templates/helpers/listbox.php
+++ code/administrator/components/com_news/templates/helpers/listbox.php
@@ -14,4 +14,40 @@ class ComNewsTemplateHelperListbox extends ComDefaultTemplateHelperListbox
 
 		return parent::_listbox($config);
 	}
+	
+	public function booleanlist( $config = array())
+	{
+		$config = new KConfig($config);
+		$config->append(array(
+			'name'   	=> '',
+			'attribs'	=> array(),
+			'true'		=> 'yes',
+			'false'		=> 'no',
+			'selected'	=> null,
+			'translate'	=> true
+		));
+		
+		$name    = $config->name;
+		$attribs = KHelperArray::toString($config->attribs);
+		
+		$html  = array();
+		
+		$extra = !$config->selected ? 'checked="checked"' : '';
+		$text  = $config->translate ? JText::_( $config->false ) : $config->false;
+		
+		$html[] = '<label class="radio" for="'.$name.'0">';
+		$html[] = '<input type="radio" name="'.$name.'" id="'.$name.'0" value="0" '.$extra.' '.$attribs.' />';
+		$html[] = '<span>'.$text.'</span>';
+		$html[] = '</label>';
+		
+		$extra = $config->selected ? 'checked="checked"' : '';
+		$text  = $config->translate ? JText::_( $config->true ) : $config->true;
+		
+		$html[] = '<label class="radio" for="'.$name.'1">';
+		$html[] = '<input type="radio" name="'.$name.'" id="'.$name.'1" value="1" '.$extra.' '.$attribs.' />';
+		$html[] = '<span>'.$text.'</span>';
+		$html[] = '</label>';
+		
+		return implode(PHP_EOL, $html);
+	}
 }
--- code/components/com_news/aliases.php
+++ code/components/com_news/aliases.php
@@ -3,3 +3,5 @@ KService::setAlias('com://site/news.database.table.articles', 'com://admin/news.
 
 KService::setAlias('com://site/news.model.articles', 'com://admin/news.model.articles');
 KService::setAlias('com://site/news.model.comments', 'com://admin/news.model.comments');
+
+KService::setAlias('com://site/news.template.helper.listbox', 'com://admin/news.template.helper.listbox');
