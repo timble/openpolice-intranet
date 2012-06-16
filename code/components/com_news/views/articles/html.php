17eabe27b41366e10590c9ce30b07882adef0178
Steven Rombauts <steven@timble.net>
2012-06-16 16:23:57 +0200

    re #1 - set document title on articles

--- code/components/com_news/views/articles/html.php
+++ code/components/com_news/views/articles/html.php
@@ -14,6 +14,35 @@ class ComNewsViewArticlesHtml extends ComNewsViewHtml
     {
         $model = $this->getModel();        
                 
+        $document = JFactory::getDocument();
+        
+        $parts = array();
+        if($model->getState()->subscribed)
+        {
+        	$parts[] = JText::_('My Articles');
+        }
+        
+        if($model->getState()->category)
+        {
+	        $category = $this->getService('com://site/news.model.categories')
+	        				->id($model->getState()->category)
+	        				->getItem();
+	        
+	        $parts[] = $category->title;
+        }
+        
+        if(count($parts)) {
+        	$document->setTitle(implode(': ', $parts));
+        }
+        
+        if($model->getState()->search)
+        {
+        	$document->setTitle(JText::_('Article Search') . ': '. $model->getState()->search);
+        }
+        elseif(!$model->getState()->subscribed && !$model->getState()->category) {
+        	$document->setTitle(JText::_('Articles'));
+        }
+
         return parent::display();
     }
 }
