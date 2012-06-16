4d542357fc9ff95cdfaa3a0702e3dfb74b613caa
Tom Janssens <tom@timble.net>
2012-06-16 19:37:51 +0200

    re #15 Added scopebar to view.events

--- code/components/com_calendar/aliases.php
+++ code/components/com_calendar/aliases.php
@@ -1,3 +1,5 @@
 <?
 
 KService::setAlias('com://site/calendar.database.table.events', 'com://admin/calendar.database.table.events');
+
+KService::setAlias('com://site/calendar.template.helper.behavior', 'com://site/news.template.helper.behavior');
