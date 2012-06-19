dadf9eff4344286933e9d9055bc540c6f43444f5
Tom Janssens <tom@timble.net>
2012-06-19 20:40:01 +0200

    re #15 Typo

--- code/components/com_calendar/views/event/tmpl/form_scopebar.php
+++ code/components/com_calendar/views/event/tmpl/form_scopebar.php
@@ -1,7 +1,6 @@
 <div class="btn-toolbar pull-right">
 	<span class="btn-group">
 		<input class="btn" type="submit" value="<?= @text('Save') ?>" />
-		<a class="btn" href="#"><?= @text('Delete') ?></a>
 		<a class="btn" href="#" class="cancel">Cancel</a>
 	</span>
 </div>
