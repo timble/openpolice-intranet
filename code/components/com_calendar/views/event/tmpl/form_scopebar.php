a8667b50eb9a3b0da202845db79dbb828f0ecccd
Tom Janssens <tom@timble.net>
2012-06-19 20:46:26 +0200

    re #15 Removed delete button

--- code/components/com_calendar/views/event/tmpl/form_scopebar.php
+++ code/components/com_calendar/views/event/tmpl/form_scopebar.php
@@ -1,7 +1,6 @@
 <div class="btn-toolbar pull-right" id="event-form-toolbar">
 	<span class="btn-group">
 		<input class="btn" type="submit" value="<?= @text('Save') ?>" />
-		<a class="btn" href="#"><?= @text('Delete') ?></a>
 		<a class="btn cancel" href="#">Cancel</a>
 	</span>
 </div>
