9f1b603c938288ca611c6499e9b8238ef71f7686
Tom Janssens <tom@timble.net>
2012-06-20 17:47:14 +0200

    Cleanup

--- code/components/com_news/views/article/tmpl/form.php
+++ code/components/com_news/views/article/tmpl/form.php
@@ -18,54 +18,52 @@
     }));
 </script>
 
-<form action="" method="post" class="-koowa-form" enctype="multipart/form-data">
+<form id="news-article-form" action="" method="post" class="-koowa-form" enctype="multipart/form-data">
 	<input type="hidden" name="action" value="save" />
 	<input type="hidden" name="news_category_id" value="<?= $state->category ?>" />
 	
 	<div class="component-header">
 		<div class="btn-toolbar pull-right">
 			<div class="btn-group">
-				<input class="btn" type="submit" value="<?= @text('Save') ?>" /><a class="btn" href="#" class="cancel">Cancel</a>
+				<input class="btn" type="submit" value="<?= @text('Save') ?>" /><a class="btn cancel" href="#">Cancel</a>
 			</div>
 		</div>
 	</div>
 	
-	<div id="news-article-form">
-		<div class="article article-form">
-	        <div class="row-fluid">
-	            <div class="span9" style="padding: 20px;">
-				   	<input type="text" name="title" class="required" value="<?= @escape($article->title) ?>" placeholder="<?= @text('Title') ?>" />
-	                <?
-	                    $controller = @service('com://admin/editors.controller.editor');
-	                    $controller->getView()->setEditorSettings($editor_settings);
-	                    echo $controller->name('text')->data($article->text)->toggle(false)->codemirror(false)->display();
-	                ?>
-	                
-	                <hr />
-	                
-	                <div class="clearfix attachments">
-	                    <? if ($article->id) : ?>
-	                        <?= @template('com://site/news.view.attachments.default') ?>
-	                    <? endif ?>
-	                    <?= @template('com://admin/attachments.view.attachments.upload') ?>
-	                </div>
-			    </div>
-			    
-			    <div class="span3">
-			        <div class="control-group">
-			        	<label class="control-label" for="news_category_id"><?= @text( 'Category' ); ?>:</label>
-			        </div>
-			    	<div class="controls">
-			    		<?= @helper('listbox.radiolist', array(
-			    				'list'     => @service('com://site/news.model.categories')->sort('title')->getList(),
-			    				'selected' => $article->news_category_id,
-			    				'name'     => 'news_category_id',
-			    		        'text'     => 'title',
-			    			));
-			    		?>
-			    	</div>
-			    </div>
+	<div class="article article-form">
+        <div class="row-fluid">
+            <div class="span9" style="padding: 20px;">
+			   	<input type="text" name="title" class="required" value="<?= @escape($article->title) ?>" placeholder="<?= @text('Title') ?>" />
+                <?
+                    $controller = @service('com://admin/editors.controller.editor');
+                    $controller->getView()->setEditorSettings($editor_settings);
+                    echo $controller->name('text')->data($article->text)->toggle(false)->codemirror(false)->display();
+                ?>
+                
+                <hr />
+                
+                <div class="clearfix attachments">
+                    <? if ($article->id) : ?>
+                        <?= @template('com://site/news.view.attachments.default') ?>
+                    <? endif ?>
+                    <?= @template('com://admin/attachments.view.attachments.upload') ?>
+                </div>
 		    </div>
-		</div>
+		    
+		    <div class="span3">
+		        <div class="control-group">
+		        	<label class="control-label" for="news_category_id"><?= @text( 'Category' ); ?>:</label>
+		        </div>
+		    	<div class="controls">
+		    		<?= @helper('listbox.radiolist', array(
+		    				'list'     => @service('com://site/news.model.categories')->sort('title')->getList(),
+		    				'selected' => $article->news_category_id,
+		    				'name'     => 'news_category_id',
+		    		        'text'     => 'title',
+		    			));
+		    		?>
+		    	</div>
+		    </div>
+	    </div>
 	</div>
 </form>
